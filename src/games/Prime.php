fb845e326175012e240d700092a7d8d878b98361
artosepyan <artyomosepyan@mail.ru>
2024-05-28 23:53:44 +0300

    Fixed Prime.php

--- src/games/Prime.php
+++ src/games/Prime.php
@@ -14,34 +14,22 @@ function prime()
     for ($j = 0; $j < 3; $j++) {
         $number = rand(1, 100);
         $questions[] = $number;
-        switch (true) {
-            case $number === 1:
-                $answers[] = 'no';
-                break;
-            case $number === 2:
-                $answers[] = 'yes';
-                break;
-            case $number % 2 === 0:
-                $answers[] = 'no';
-                break;
-            case $number % 5 === 0:
-                $answers[] = 'no';
-                break;
-            case $number % 3 === 0:
-                $answers[] = 'no';
-                break;
-            case $number % 11 === 0:
-                $answers[] = 'no';
-                break;
-            case $number % 7 === 0:
-                $answers[] = 'no';
-                break;
-            case $number % 13 === 0:
-                $answers[] = 'no';
-                break;
-            default:
-                $answers[] = 'yes';
+        if ($number === 1) {
+            $answers[] = 'no';
+            continue;
+        } elseif ($number === 2) {
+            $answers[] = 'yes';
+            continue;
         }
+        $sqrtNumber = sqrt($number);
+        $temp = null;
+        for ($i = 2; $i <= $sqrtNumber; $i++) {
+            if ($number % $i === 0) {
+                $temp = 'no'; 
+                break;
+            }
+        }
+        $answers[] = $temp ?? 'yes';
     }
     line('Answer "yes" if given number is prime. Otherwise answer "no".');
     engine($questions, $answers, $name);
