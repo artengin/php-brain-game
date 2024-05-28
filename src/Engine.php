1525034f4c7abfa3a77ad2c1274597a17b832c94
artosepyan <artyomosepyan@mail.ru>
2024-05-28 23:26:00 +0300

    addet type parameter function in Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -5,7 +5,7 @@ namespace BrainGames\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function engine($question, $correctAnswer, $name)
+function engine(array $question, array $correctAnswer, string $name)
 {
     for ($i = 0, $l = count($question); $i < $l; $i++) {
         line("Question: {$question[$i]}");
